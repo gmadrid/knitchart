460b6c8535d1328275c53a4313e048163695beaf
George Madrid <gmadrid@gmail.com>
2019-12-21 17:59:11 -0500

    Attribute attribute seems to be working.

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,112 +1,169 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use quote::{quote};
-use syn::{parse_macro_input, Attribute, Data, DeriveInput, Field, Ident, Type, Meta, NestedMeta, Lit};
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput, Field, Ident, Lit, Meta, NestedMeta};
 
 use proc_macro2::TokenStream as TokenStream2;
 
 /*
-  #[derive(Attributes)] will derive two things for the tagged struct:
-  - the Default trait
-  - a method, set_value(&mut self, name: &str, value: &str).
+ #[derive(Attributes)] will derive two things for the tagged struct:
+ - the Default trait
+ - a method, set_value(&mut self, name: &str, value: &str).
 
-  Each field may be tagged with the 'attr' attribute which may contain
-  the following sub-attributes:
-    default = a string literal that will be parsed to set the default value of that field.
-    parse = a method that will be called to parse the string into the field value.
+ Each field may be tagged with the 'attr' attribute which may contain
+ the following sub-attributes:
+   default = a string literal that will be parsed to set the default value of that field.
+   parse = a method that will be called to parse the string into the field value.
 
-  E.g.:
+ E.g.:
 
-      #[derive(Attributes)]
-      pub struct Attributes {
+     #[derive(Attributes)]
+     pub struct Attributes {
 
-        rows: usize,  // default will be set with usize::default()
+       rows: usize,  // default will be set with usize::default()
 
-        #[attr(64)]
-        cols: usize,
+       #[attr(64)]
+       cols: usize,
 
-        #[attr('.', parse=parse_char_name)]
-        knit_char: char,  // but uses special parser function
+       #[attr('.', parse=parse_char_name)]
+       knit_char: char,  // but uses special parser function
 
-        #[attr("whitesmoke")]
-        color: CssColor,  // still using s.parse()?
-      }
- */
+       #[attr("whitesmoke")]
+       color: CssColor,  // still using s.parse()?
+     }
+*/
 
 #[derive(Debug, Default)]
 struct AttrMeta {
-    default_string: Option<String>
+    default_string: Option<String>,
+    parse_func: Option<Ident>,
 }
 
 impl AttrMeta {
     fn meta_for_field(field: &Field) -> AttrMeta {
-	let my_attr = field.attrs.iter().find(|a| {
-	    if let Some(segment) = a.path.segments.first() {
-		if segment.ident.to_string() == "attr" {
-		    return true;
-		}
-	    }
-	    false
-	});
+        let my_attr = field.attrs.iter().find(|a| {
+            if let Some(segment) = a.path.segments.first() {
+                if segment.ident == "attr" {
+                    return true;
+                }
+            }
+            false
+        });
 
-	let mut attr_meta = AttrMeta::default();
-	my_attr.map(|attr| {
-	    if let Ok(Meta::List(metalist)) = attr.parse_meta() {
-		if let Some(NestedMeta::Meta(nested)) = metalist.nested.first() {
-		    if let Meta::NameValue(name_value) = nested {
-			if name_value.path.segments.first().map_or(false, |s| s.ident.to_string() == "default" ) {
-			    if let Lit::Str(def) = &name_value.lit {
-				attr_meta.default_string = Some(def.value());
-			    }
-			    // TODO: add better error checking.
-			}
-		    }
-		}
-	    }
-	});
-	attr_meta
+        let mut attr_meta = AttrMeta::default();
+        my_attr.map(|attr| {
+            if let Ok(Meta::List(metalist)) = attr.parse_meta() {
+                for nested_meta in &metalist.nested {
+                    if let NestedMeta::Meta(meta) = nested_meta {
+                        if let Meta::NameValue(name_value) = meta {
+                            match name_value.path.segments.first().map(|s| &s.ident) {
+                                Some(i) if i == "default" => {
+                                    if let Lit::Str(def) = &name_value.lit {
+                                        attr_meta.default_string = Some(def.value())
+                                    } else {
+                                        unreachable!();
+                                    }
+                                }
+                                Some(i) if i == "parse" => {
+                                    if let Lit::Str(parse) = &name_value.lit {
+                                        let parse_ident = Ident::new(&parse.value(), parse.span());
+                                        attr_meta.parse_func = Some(parse_ident);
+                                    } else {
+                                        unreachable!()
+                                    }
+                                }
+                                // Do you want to report unknown attrs?
+                                _ => { /* no-op */ }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        attr_meta
     }
 }
 
-fn attr_attribute(field: &Field) -> Option<&Attribute> {
-    field.attrs.iter().find(|a| {
-	if let Some(segment) = a.path.segments.first() {
-	    if segment.ident.to_string() == "attr" {
-		return true;
-	    }
-	}
-	false
-    })
-}
-
-fn default_value(field: &Field) {
-    let a = attr_attribute(field);
-}
-
-fn make_default_trait(struct_name: &Ident,
-		      fields: &Vec<&Field>,
-		      field_names: &Vec<&Ident>,
-		      types: &Vec<&Type>,
+fn make_default_trait(
+    struct_name: &Ident,
+    fields: &Vec<&Field>,
+    field_names: &Vec<&Ident>,
 ) -> TokenStream2 {
-    default_value(fields[1]);
-    let meta_for_fields = fields.iter().map(|f| AttrMeta::meta_for_field(f)).collect::<Vec<_>>();
+    let meta_for_fields = fields
+        .iter()
+        .map(|f| AttrMeta::meta_for_field(f))
+        .collect::<Vec<_>>();
 
-    let default_exprs = meta_for_fields.iter().map(|am| {
-	am.default_string.as_ref().map_or_else(|| { quote!{ std::default::Default::default() } },
-				     |val| quote! { #val.parse().unwrap() })
-    }).collect::<Vec<_>>();
-    
+    let default_exprs = meta_for_fields
+        .iter()
+        .map(|am| {
+            am.default_string.as_ref().map_or_else(
+                || {
+                    quote! { std::default::Default::default() }
+                },
+                |val| {
+                    if let Some(parse_func) = &am.parse_func {
+                        quote! { #parse_func(#val) }
+                    } else {
+                        quote! { #val.parse().unwrap() }
+                    }
+                },
+            )
+        })
+        .collect::<Vec<_>>();
+
     let q = quote! {
-	impl Default for #struct_name {
-	    fn default() -> #struct_name {
-		#struct_name {
-		    #(#field_names: #default_exprs),*
-		}
-	    }
-	}
+    impl Default for #struct_name {
+        fn default() -> #struct_name {
+        #struct_name {
+            #(#field_names: #default_exprs),*
+        }
+        }
+    }
     };
     q
+}
+
+fn make_set_value_func(
+    struct_name: &Ident,
+    fields: &Vec<&Field>,
+    field_names: &Vec<&Ident>,
+) -> TokenStream2 {
+    // TODO stop making this twice.
+    let meta_for_fields = fields
+        .iter()
+        .map(|f| AttrMeta::meta_for_field(f))
+        .collect::<Vec<_>>();
+
+    let parse_exprs = meta_for_fields
+        .iter()
+        .map(|am| {
+            if let Some(parse_func) = &am.parse_func {
+                quote! { #parse_func(v) }
+            } else {
+                quote! { v.parse().unwrap() }
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let if_statements = quote! {
+        #(
+        if std::stringify!(#field_names) == n {
+            self.#field_names = #parse_exprs;
+            return;
+        }
+        )*
+    };
+
+    let q = quote! {
+    impl #struct_name {
+        fn set_value(&mut self, n: &str, v: &str) {
+        #if_statements
+        }
+    }
+    };
+    q.into()
 }
 
 #[proc_macro_derive(Attributes, attributes(attr))]
@@ -114,19 +171,26 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
     let fields = match input.data {
-	Data::Struct(ref data_struct) => {
-	    data_struct.fields.iter().filter(|f| f.ident.is_some()).collect::<Vec<&Field>>()
-	},
-	// TODO: produce a better error message.
-	_ => panic!("This is only for structs.")
+        Data::Struct(ref data_struct) => data_struct
+            .fields
+            .iter()
+            .filter(|f| f.ident.is_some())
+            .collect::<Vec<&Field>>(),
+        // TODO: produce a better error message.
+        _ => panic!("This is only for structs."),
     };
-    let field_names = fields.iter().map(|f| f.ident.as_ref().unwrap()).collect::<Vec<&Ident>>();
-    let types = fields.iter().map(|f| &f.ty).collect::<Vec<&Type>>();
+    let field_names = fields
+        .iter()
+        .map(|f| f.ident.as_ref().unwrap())
+        .collect::<Vec<&Ident>>();
+    let default_trait = make_default_trait(&input.ident, &fields, &field_names);
 
-    let default_trait = make_default_trait(&input.ident, &fields, &field_names, &types);
+    let set_value_func = make_set_value_func(&input.ident, &fields, &field_names);
 
-    let q = quote!{
-	#default_trait
+    let q = quote! {
+    #default_trait
+
+    #set_value_func
     };
 
     q.into()
--- macros/src/main.rs
+++ macros/src/main.rs
@@ -4,14 +4,27 @@ use macros::Attributes;
 #[derive(Attributes, Debug)]
 pub struct Attributes {
     rows: usize,
-    #[attr(default="64")]
+    #[attr(default = "64")]
     cols: usize,
-    #[attr(default="."/*, parse=parse_char_name*/)]
-    knit_char: char,  // but uses special parser function
-    #[attr(default="whitesmoke")]
-    color: CssColor,  // still using s.parse()?
+    #[attr(default = ".", parse = "parse_char_name")]
+    knit_char: char, // but uses special parser function
+    #[attr(default = "whitesmoke")]
+    color: CssColor,
+}
+
+fn parse_char_name(s: &str) -> char {
+    if s == "SPACE" {
+        ' '
+    } else {
+        s.parse().unwrap()
+    }
 }
 
 fn main() {
-    eprintln!("The attributes: {:?}", Attributes::default());
+    let mut attrs = Attributes::default();
+
+    attrs.set_value("rows", "18");
+    attrs.set_value("knit_char", "SPACE");
+
+    eprintln!("The attributes: {:?}", attrs);
 }
